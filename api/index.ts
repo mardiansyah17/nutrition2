404bd97ed859c028f2bc8b7f39a0d67255a78fe5
Muhammad Mardinasyah <mardiansyahm002@gmail.com>
2024-06-05 10:32:08 +0700

    up

--- api/index.ts
+++ api/index.ts
@@ -1,1 +1,8 @@
-console.log("cakep");
+import express from "express";
+
+const router = express.Router();
+router.get("/", (req, res) => {
+  return res.send("cakep bro");
+});
+
+export default router;
